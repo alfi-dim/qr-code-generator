426d25bbcc12eb4a9aefb924c6c22282ccd7c986
Dimas Alfiansyah <alfi.dim1612@gmail.com>
2025-11-28 13:19:36 +0700

    feat(components): implement AppSidebar and additional UI components
    - Added new `AppSidebar` component with dynamic navigation, user, and project menus
    - Implemented reusable components: `Avatar`, `Breadcrumb`, `Collapsible`, `Input`, `Label`, `SearchForm`, `Separator`, `Sheet`, and `Sidebar`
    - Integrated navigation components: `NavMain`, `NavProjects`, `NavSecondary`, and `NavUser`
    - Enhanced accessibility and interactivity using Radix UI primitives

---
+++ src/app/components/site-header.tsx
@@ -0,0 +1,43 @@
+import {SidebarIcon} from "lucide-react";
+
+import {ThemeDropdown} from "@/app/components/ThemeDropdown";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/app/components/ui/breadcrumb";
+import {Button} from "@/app/components/ui/button";
+import {Separator} from "@/app/components/ui/separator";
+import {useSidebar} from "@/app/components/ui/sidebar";
+
+export function SiteHeader() {
+  const {toggleSidebar} = useSidebar();
+
+  return (
+    <header className="bg-background sticky top-0 z-50 flex w-full items-center border-b">
+      <div className="flex h-(--header-height) w-full items-center gap-2 px-4">
+        <Button className="h-8 w-8" variant="ghost" size="icon" onClick={toggleSidebar}>
+          <SidebarIcon/>
+        </Button>
+        <Separator orientation="vertical" className="mr-2 h-4"/>
+        <Breadcrumb className="hidden sm:block">
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <BreadcrumbLink href="#">Building Your Application</BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator/>
+            <BreadcrumbItem>
+              <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+        <div className="ml-auto">
+          <ThemeDropdown/>
+        </div>
+      </div>
+    </header>
+  );
+}
